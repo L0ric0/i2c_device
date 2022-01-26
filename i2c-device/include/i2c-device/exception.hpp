209ae3a9be0f916c3d7f0371b202067e84d20a96
Lorenz Steinert <lorenz@steinerts.de>
2022-01-27 00:51:11 +0100

    fix generation of dll export header and building of static/dynamic libs

--- i2c-device/include/i2c-device/exception.hpp
+++ i2c-device/include/i2c-device/exception.hpp
@@ -1,13 +1,21 @@
 #ifndef I2C_EXCEPTION_HPP_
 #define I2C_EXCEPTION_HPP_
 
+#include "i2c-device/detail/i2c-device_export.h"
+
+
 #include <system_error>
 #include <string>
 
 namespace i2c::exception
 {
-
-	struct i2c_error : std::system_error {
+    // suppress C4275 (non dll-interface class 'std::system_error' used as base for dll-interface struct 'i2c::exception::i2c_error')
+    // to not end in a compiler error as the common consensus seems to be to just ignore it
+    // https://stackoverflow.com/questions/24511376/how-to-dllexport-a-class-derived-from-stdruntime-error
+    // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=msvc-170
+	struct I2C_DEVICE_EXPORT i2c_error : std::system_error
+    #pragma warning(suppress : 4275)
+    {
         i2c_error(std::error_code ec);
         i2c_error(std::error_code ec, const std::string& what_arg);
         i2c_error(std::error_code ec, const char* what_arg);
@@ -16,43 +24,43 @@ namespace i2c::exception
         i2c_error(int ev, const std::error_category& ecat, const char* what_arg);
     };
 
-    struct again : i2c_error {
+    struct I2C_DEVICE_EXPORT again : i2c_error {
         again(int ev = EAGAIN, const std::error_category& ecat = std::system_category());
     };
 
-    struct bad_message : i2c_error {
+    struct I2C_DEVICE_EXPORT bad_message : i2c_error {
         bad_message(int ev = EBADMSG, const std::error_category& ecat = std::system_category());
     };
 
-    struct busy : i2c_error {
+    struct I2C_DEVICE_EXPORT busy : i2c_error {
         busy(int ev = EBUSY, const std::error_category& ecat = std::system_category());
     };
 
-    struct invalid_parameter : i2c_error {
+    struct I2C_DEVICE_EXPORT invalid_parameter : i2c_error {
         invalid_parameter(int ev = EINVAL, const std::error_category& ecat = std::system_category());
     };
 
-    struct io_error : i2c_error {
+    struct I2C_DEVICE_EXPORT io_error : i2c_error {
         io_error(int ev = EIO, const std::error_category& ecat = std::system_category());
     };
 
-    struct no_device : i2c_error {
+    struct I2C_DEVICE_EXPORT no_device : i2c_error {
         no_device(int ev = ENODEV, const std::error_category& ecat = std::system_category());
     };
 
-    struct no_device_or_address : i2c_error {
+    struct I2C_DEVICE_EXPORT no_device_or_address : i2c_error {
         no_device_or_address(int ev = ENXIO, const std::error_category& ecat = std::system_category());
     };
 
-    struct operation_not_supported : i2c_error {
+    struct I2C_DEVICE_EXPORT operation_not_supported : i2c_error {
         operation_not_supported(int ev = EOPNOTSUPP, const std::error_category& ecat = std::system_category());
     };
 
-    struct protocol_error : i2c_error {
+    struct I2C_DEVICE_EXPORT protocol_error : i2c_error {
         protocol_error(int ev = EPROTO, const std::error_category& ecat = std::system_category());
     };
 
-    struct timedout : i2c_error {
+    struct I2C_DEVICE_EXPORT timedout : i2c_error {
         timedout(int ev = ETIMEDOUT, const std::error_category& ecat = std::system_category());
     };
 }
