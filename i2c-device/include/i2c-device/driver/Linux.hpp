3ab775d51edb857b7bad52b10b398ee3d1b573b6
Lorenz Steinert <lorenz@steinerts.de>
2022-02-15 14:13:51 +0100

    make multiple methods const in the linux impl

--- i2c-device/include/i2c-device/driver/Linux.hpp
+++ i2c-device/include/i2c-device/driver/Linux.hpp
@@ -46,7 +46,7 @@ namespace i2c::driver
             ::fclose(handle);
         }
 
-        void handle_i2c_error(const int ret_val)
+        void handle_i2c_error(const int ret_val) const
         {
             if (ret_val > 0) {
                 return;
@@ -80,7 +80,7 @@ namespace i2c::driver
             }
         }
 
-        void write(const handle_type& handle, std::contiguous_iterator auto buffer_begin, std::contiguous_iterator auto buffer_end)
+        void write(const handle_type& handle, std::contiguous_iterator auto buffer_begin, std::contiguous_iterator auto buffer_end) const
         {
             const auto bytes_to_write = std::distance(buffer_begin, buffer_end);
             const auto res = ::write(handle, &(*buffer_begin), bytes_to_write);
@@ -89,7 +89,7 @@ namespace i2c::driver
         }
 
         template<byte T>
-        void read(const handle_type& handle, contiguous_output_iterator<T> auto buffer_begin, contiguous_output_iterator<T> auto buffer_end)
+        void read(const handle_type& handle, contiguous_output_iterator<T> auto buffer_begin, contiguous_output_iterator<T> auto buffer_end) const
         {
             const auto bytes_to_read = std::distance(buffer_begin, buffer_end);
 
@@ -104,40 +104,40 @@ namespace i2c::driver
             handle_i2c_error(res);
         }
 
-        std::uint8_t read_byte(const handle_type& handle)
+        std::uint8_t read_byte(const handle_type& handle) const
         {
             const auto res = ::i2c_smbus_read_byte(handle);
             handle_i2c_error(res);
             return static_cast<std::uint8_t>(res);
         }
 
-        void write_byte(const handle_type& handle, const std::uint8_t value)
+        void write_byte(const handle_type& handle, const std::uint8_t value) const
         {
             const auto res = ::i2c_smbus_write_byte(handle, value);
             handle_i2c_error(res);
         }
 
-        std::uint8_t read_byte_data(const handle_type& handle, const std::uint8_t reg)
+        std::uint8_t read_byte_data(const handle_type& handle, const std::uint8_t reg) const
         {
             const auto res = ::i2c_smbus_read_byte_data(handle, reg);
             handle_i2c_error(res);
             return static_cast<std::uint8_t>(res);
         }
 
-        void write_byte_data(const handle_type handle, const std::uint8_t reg, const std::uint8_t value)
+        void write_byte_data(const handle_type handle, const std::uint8_t reg, const std::uint8_t value) const
         {
             const auto res = ::i2c_smbus_write_byte_data(handle, reg, value);
             handle_i2c_error(res);
         }
 
-        std::uint16_t read_word_data(const handle_type& handle, const std::uint8_t reg)
+        std::uint16_t read_word_data(const handle_type& handle, const std::uint8_t reg) const
         {
             const auto res = ::i2c_smbus_read_word_data(handle, reg);
             handle_i2c_error(res);
             return util::from_device_repr<std::uint16_t, Endian>(static_cast<std::uint16_t>(res));
         }
 
-        void write_word_data(const handle_type handle, const std::uint8_t reg, const std::uint16_t value)
+        void write_word_data(const handle_type handle, const std::uint8_t reg, const std::uint16_t value) const
         {
             const auto res = ::i2c_smbus_write_word_data(handle, reg, util::to_device_repr<std::uint16_t, Endian>(value));
             handle_i2c_error(res);
@@ -148,7 +148,7 @@ namespace i2c::driver
             const handle_type& handle,
             const std::uint8_t reg,
             contiguous_output_iterator<T> auto buffer_begin,
-            contiguous_output_iterator<T> auto buffer_end)
+            contiguous_output_iterator<T> auto buffer_end) const
         {
             i2c_smbus_read_i2c_block_data(handle, reg, buffer_begin, buffer_end);
         }
